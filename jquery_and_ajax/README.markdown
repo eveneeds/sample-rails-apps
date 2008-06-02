04bcd5c3df89b9144aa5da8c0623d2877ec0d0ac
August Lilleaas <augustlilleaas@gmail.com>
2008-06-02 13:18:43 +0200

    Added a (pretty much blank) rails app for jquery and ajax examples

---
+++ jquery_and_ajax/README.markdown
@@ -0,0 +1,3 @@
+# jQuery and Ajax examples
+
+Shows how to do unobtrusive javascript with jQuery. jQuery makes it easy to write unobtrusive javascript, which isn't the case with Rails' built-in javascript helpers (link_to_remote etc). Unobtrusive javascript is good, as it makes your webapps work for people that doesn't have javascript. It's also a nice way to clean up your javascripts, making it more maintainable.
---
+++ jquery_and_ajax/app/controllers/application.rb
@@ -0,0 +1,15 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '53e1f67a7f94178f3ec83914d882a799'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
+end

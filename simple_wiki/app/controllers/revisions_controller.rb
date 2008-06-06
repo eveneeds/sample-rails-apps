8cff6b856b34a53e75447e646adc6ac101159062
August Lilleaas <augustlilleaas@gmail.com>
2008-06-07 00:02:38 +0200

    Showing the revision number instead of the id in the revisions controller

--- simple_wiki/app/controllers/revisions_controller.rb
+++ simple_wiki/app/controllers/revisions_controller.rb
@@ -1,6 +1,6 @@
 class RevisionsController < ApplicationController
   def show
     @page = Page.find(params[:page_id])
-    @revision = @page.revisions.find(params[:id])
+    @revision = @page.revisions.find_by_number(params[:id])
   end
 end
--- simple_wiki/test/functional/revisions_controller_test.rb
+++ simple_wiki/test/functional/revisions_controller_test.rb
@@ -8,4 +8,15 @@ class RevisionsControllerTest < ActionController::TestCase
     get :show, :page_id => page.to_param, :id => 1
     assert_response :success
   end
+  
+  def test_finding_by_number_not_id
+    # Creating revisions so that the primary key doesn't start at 1
+    Page.create(:title => 'New page yay', :content => "yep").update_attributes(:title => "Edited totally liek")
+    
+    page = pages(:rails)
+    page.update_attributes :title => "Changed"
+    get :show, :page_id => page.to_param, :id => 1
+    assert_response :success
+    assert_equal page.latest_revision, assigns(:revision)
+  end
 end

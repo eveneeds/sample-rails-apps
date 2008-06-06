ec7775b0f14b9c775128d593492f27f33b728b6d
August Lilleaas <augustlilleaas@gmail.com>
2008-06-06 23:56:51 +0200

    Making sure the textile helper uses the normal textile rules as well

--- simple_wiki/app/helpers/application_helper.rb
+++ simple_wiki/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def textilize(text)
-    TextileCloth.parse(text, new_page_path) {|page| url_for(page_path(page)) }.to_html(:inline_textile_pagelink)
+    TextileCloth.parse(text, new_page_path) {|page| url_for(page_path(page)) }.to_html(*(TextileCloth::DEFAULT_RULES << :inline_textile_pagelink))
   end
 end

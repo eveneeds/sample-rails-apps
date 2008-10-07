25baf6e48a4fae6dfa1e343b93d45119132dc58f
August Lilleaas <augustlilleaas@gmail.com>
2008-10-07 10:32:52 +0200

    Storing a generated salt in the database, rather than a static, global salt. If we use the same salt for all users, users with identical passwords will be able to see that their passwords are identical by looking at the password hashes (if they get access to them, for some reason).

--- authentication/app/models/user.rb
+++ authentication/app/models/user.rb
@@ -1,30 +1,33 @@
 require 'digest/sha1'
 class User < ActiveRecord::Base
-  # It is important that the salt is secret. Generate your own with `rake secret`.
-  SALT = "a6ab20a65296b2db942944ef7820e1bc74cabb96"
   validates_length_of :password, :minimum => 4, :if => :password_required?
   before_save :encrypt_password
   attr_accessor :password
   
   # Returns a User if the username and password matches, nil if not.
   def self.authenticate(username, password)
-    User.find_by_username_and_password_hash(username, encrypt_password(password))
+    user = User.find_by_username(username)
+    user ? (user.password_hash == encrypt_password(password, user.password_salt) && user) : nil
   end
   
   private
   
-  # Automatically encrypt the 'password' if it's set. Don't do anything if the
-  # 'password' hasn't changed, unless it's a new record (see password_required?)
   def encrypt_password
     return unless password_required?
-    self.password_hash = self.class.encrypt_password(self.password)
+    self.password_salt = generate_salt
+    self.password_hash = self.class.encrypt_password(password, password_salt)
   end
   
   # Uses Digest::SHA1 from the Ruby standard library and performs a one-way encryption
   # of the password. The password is also salted. Read more about salting here:
   # http://www.aspheute.com/english/20040105.asp
-  def self.encrypt_password(password)
-    Digest::SHA1.hexdigest("#{password}#{SALT}")
+  def self.encrypt_password(password, salt)
+    Digest::SHA1.hexdigest("--#{password}--||--#{salt}--")
+  end
+  
+  # Generates a fairly random salt string.
+  def generate_salt
+    Digest::SHA1.hexdigest(Time.now.to_s.split(//).sort_by { rand }.join)
   end
   
   # See encrypt_password.
--- authentication/db/migrate/20080919202321_create_users.rb
+++ authentication/db/migrate/20080919202321_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string :username, :password_hash
+      t.string :username, :password_hash, :password_salt
       t.timestamps
     end
   end
